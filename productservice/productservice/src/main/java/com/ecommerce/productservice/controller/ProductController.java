9a5f4e1121b310fc8024626cff65815f6beaa555
vasanttth <vasanthansanthavel@gmail.com>
2026-05-26 13:53:56 +0530

    Allow Netlify frontend CORS

--- productservice/productservice/src/main/java/com/ecommerce/productservice/controller/ProductController.java
+++ productservice/productservice/src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     private final ProductService productService;
